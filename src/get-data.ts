4e372cf4a271aca2b2bc8fbdf1f782ef3ec1e84a
Matthieu Gay-Bellile <matthieu.gb@gatech.edu>
2018-10-20 20:47:24 -0400

    Fix following

--- src/get-data.ts
+++ src/get-data.ts
@@ -4,7 +4,7 @@ const URL = "https://api.twitch.tv/kraken/streams/";
 const clientId = "n64yujbrc3geoobvcwnr427qqcuzq8";
 
 function buildUrl(offset) {
-    return URL + "?client_id=" + clientId + 
+    return URL + "?client_id=" + clientId +
         "&limit=5" +
         ((offset != null) ? "&offset=" + offset : "");
 }
@@ -26,23 +26,24 @@ class GetData {
     }
 
     followStream(channelId) {
-        this.getUserId().then((userId)=>{
-            console.log(userId);
-            return fetch("https://api.twitch.tv/kraken/users/" + userId._id + "/follows/channels/" + channelId, {
-            method: "PUT"
-            })
-            .then(
-                (response) => {
-                    if (response.status !== 200) {
-                        console.log("fetch failed to get data");
-                        console.log(response.status);
-                        return;
-                    }
-                    return response.json();
-                }
-            );
-        });
-        
+      return fetch(
+        "https://api.twitch.tv/kraken/users/" + window.localStorage.getItem("user_id") + "/follows/channels/" + channelId,
+        {
+          method: "PUT",
+          headers: {
+            "Accept": "application/vnd.twitchtv.v5+json",
+            "Client-ID": clientId,
+            "Authorization": "OAuth " + window.localStorage.getItem("access_token"),
+          }
+        },
+      ).then((response) => {
+        if (response.status !== 200) {
+          console.log("fetch failed to get data");
+          console.log(response.status);
+          return;
+        }
+        return response.json();
+      });
     }
 
     getUserId() {
